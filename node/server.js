28fbfda11c4e36367d742c3b9754957e7b5cdc11
Jacob Earl <jake.braul@gmail.com>
2016-04-21 10:26:55 +0800

    remove middleware

--- node/server.js
+++ node/server.js
@@ -25,9 +25,11 @@ app.set('views', __dirname + '\\app\\View\\');
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+// configure the directory
 app.use(express.static('public'));
 app.use(express.static('public/uploads'));
 app.use(express.static(__dirname + '/app/node_modules/bootstrap')); 
+// configure session in express
 app.use(session({
   cookieName: 'session',
   secret: 'jacob',
@@ -35,39 +37,49 @@ app.use(session({
   activeDuration: 86400
 }));
 
-app.get('/home', function(req,res) {
-  var userTable = [];
-  modelUser.findAll({where: {status : 1}}).then(function(users) {
-    res.render('Home/index', {users:users});
-  })
-});
-
-function hasSession(req, res, next) {
-  console.log(req.session);
-  next();
+function hasLogin(req, res, next) {
+  if (!req.session.user) {
+    res.redirect('/');
+  } else {
+    next();
+  }
 }
 
-app.get('/',  function(req, res) {
-  res.render('Home/login');
+app.get('*', function(req,res,next) {
+  console.log(req.url);
+  next();
+})
+
+app.get('/logout', function(req, res) {
+  req.session.reset();
+  res.redirect('/');
+})
+
+app.get('/', function(req, res) {
+  if (req.session.user) {
+    res.redirect('/home');
+  }else {
+    res.render('Home/login');
+  }
 })
 
 app.post('/login', function(req, res) {
-  console.log(req.body);
-  modelUser.findOne(req.body, function(err, user) {
-    if(!user) {
-      res.send('Invalid username or password');
-    } else {
-      res.send('redirect to home page..');
+  modelUser.findOne({ where: {username: req.body.username, password: req.body.password}}).then(function(user,err) {
+    if(user) {
+      req.session.user = user.dataValues;
+      res.redirect('/home');
+    }else{
+      res.redirect('/');
     }
-  })
-  /*if (req.body.username && req.body.password) {
-    req.session = req.body.user;
-    console.log(req.session);
-    res.redirect('/home');
-  } else {
-    res.redirect('/');
-  }*/
-})
+  });
+});
+
+app.get('/home', hasLogin, function(req,res) {
+  var userTable = [];
+  modelUser.findAll({where: {status : 1}}).then(function(users) {
+    res.render('Home/index', {users:users});
+  });
+});
 
 app.get('/profile', function(req,res) {
   res.writeHead(200, {"Context-Type" : "text/html"});
@@ -81,13 +93,9 @@ io.on('connection', function(socket) {
     io.emit('display_new_user', data);
   })
 
-  // saves new user
   socket.on('send_message', function(data) {
-    // insert new user in table
     modelUser.create(data).then(function(results) {  
-      // broadcast the newly saved user
       io.emit('showUser', results);
-      console.log(results);
     });
   })
 
@@ -98,19 +106,13 @@ io.on('connection', function(socket) {
     socket.emit('broadcastImage', {image: filename});
   })
 
-  // update user status
   socket.on("delete_user", function(data) {
-    // removes the user in ui
     io.emit("remove_user", data);
-    // update the user from the table
     modelUser.update({status: 0}, {where: {id: data.id}})
   });
 
-  // update user info
   socket.on('update_user', function(data) {
-    // updates the user info from the table
     modelUser.update(data, {where: {id:data.id}})
-    // broadcast the newly update user
     io.emit('show_updated_user', data);
   })
 
@@ -119,7 +121,6 @@ io.on('connection', function(socket) {
       io.emit('return_all_users', results);
     })
   })
-
 });
 
 http.listen(port, function(req) {
